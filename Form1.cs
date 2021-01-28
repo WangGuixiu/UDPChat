ce7b95ce072cc34d196b037322bbfa02892617ca
Guixiu Wang <wGX3512354405@outlook.com>
2021-01-28 15:27:31 +0800

    update

--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+
 
 namespace UDPChat
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
+        udpSend newSend = new udpSend();
+        udpRecv newRecv = new udpRecv();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,13 +25,24 @@ namespace UDPChat
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, World");
-            this.Close();
+            string message = richTextBox2.Text;
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            IPEndPoint hostRecv = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8898);
+            newSend.SendData(hostRecv, data);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Thread recvThread = new Thread(StartRecv);
+            recvThread.Start();
+        }
 
+        private void StartRecv()
+        {
+            while (true)
+            {
+                richTextBox1.Text = newRecv.RecvData();
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 
 namespace UDPChat
 {
+    struct udpData
+    {
+        public string message;
+    }
+
     static class Program
     {
         /// <summary>
@@ -17,6 +26,32 @@ namespace UDPChat
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+    }
+
+    // UDP发送数据
+    public class udpSend
+    {
+        private static IPEndPoint hostSend = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8899);
+        UdpClient udpSendSock = new UdpClient(hostSend);
+
+        public void SendData(IPEndPoint hostRecv, byte[] data)
+        {
+            udpSendSock.Send(data, data.Length, hostRecv);
+        }
+    }
+
+    // UDP接收数据
+    public class udpRecv
+    {
+        private static IPEndPoint hostRecv = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8898);
+        UdpClient udpRecvSock = new UdpClient(hostRecv);
+        byte[] data = null;
+
+        public string RecvData()
+        {
+            data = udpRecvSock.Receive(ref hostRecv);
+            return Encoding.UTF8.GetString(data);
         }
     }
 }
